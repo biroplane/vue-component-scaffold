2b18c380752b6a4d63adda3a8abeb2ff48ea5871
Biro <g.vallarelli@gmail.com>
2020-05-18 20:33:46 +0200

    now it works

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs')
+const path = require('path')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +19,70 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vue-component-scaffold.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('vue-component-scaffold.createComponentFolder', function () {
+		let name, dir
+		vscode.window.showInputBox({
+			prompt: "Component Name",
+			placeHolder: 'VueComponent'
+		}).then(str => {
+			console.log("Il nome del componente è ", n);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vue-component-scaffold!');
+			name = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+			const folderPath = vscode.workspace.workspaceFolders[0].uri.toString().split(':')[1]
+			dir = path.join(folderPath, name)
+			return dir
+		}).then((dir) => {
+			console.log("La nuova dir è ", dir)
+
+			try {
+				//if (options.folder) {
+				//creo la cartella
+				if (!fs.existsSync(dir)) {
+					fs.mkdirSync(dir)
+					console.log("%s Creating Folder 📂: \t", '✔')
+				} else {
+					console.log("%s Folder alreeady exists 📂:\t ", '⚠', name)
+				}
+
+				//}
+				let vueContent = `<template src="./${name}.html"></template>
+	<script src="./${name}.js"></script>
+	<script src="./${name}.ctrl.js"></script>
+	<style src="./${name}.scss" lang="scss"></style>`
+				let jsContent = `export default {}`
+				let scssContent = `@import variables`
+				fs.writeFileSync(path.join(dir, name + '.vue'), vueContent)
+				console.log("%s Creating Vue File 📂: \t", '✔')
+				fs.writeFileSync(path.join(dir, name + '.js'), jsContent)
+				console.log("%s Creating Js File 📂: \t", '✔')
+				fs.writeFileSync(path.join(dir, name + '.ctrl.js'), jsContent)
+				console.log("%s Creating Js Store File 📂: \t", '✔')
+				fs.writeFileSync(path.join(dir, name + '.scss'), scssContent)
+				console.log("%s Creating Scss File 📂: \t", '✔')
+				fs.writeFileSync(path.join(dir, name + '.html'), '')
+				console.log("%s Creating Html File 📂: \t", '✔')
+
+			} catch (error) {
+				console.log("errore", error)
+			}
+		})
+		// 		let vueContent = `<template src="./${name}.html"></template>
+		// <script src="./${name}.js"></script>
+		// <script src="./${name}.ctrl.js"></script>
+		// <style src="./${name}.scss" lang="scss"></style>
+		//     `
+		// 		// ? let jsContent = `export default {}`
+		// 		// ? let scssContent = `@import variables`
+		// 		console.log("Folder Path ", vscode.workspace.workspaceFolders)
+		// 		fs.writeFile(path.join(folderPath, `${name}.vue`), vueContent, err => {
+		// 			if (err) {
+		// 				console.error(err);
+		// 				vscode.window.showErrorMessage("Failed to create VUE file")
+		// 			}
+		// 			vscode.window.showInformationMessage("File Scritto correttamente ")
+		// 		})
+
+
 	});
 
 	context.subscriptions.push(disposable);
